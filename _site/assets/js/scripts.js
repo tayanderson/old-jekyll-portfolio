45101d3adc243414f3198e16c159064a6c5ed4dd
Taylor Anderson <andersts@indiana.edu>
2015-07-08 18:57:11 -0500

    initial commit

---
+++ _site/assets/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+     $(".owl-carousel").owlCarousel({
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem: true,
+        autoHeight: true
+     })
+    
+});
---
+++ assets/js/cbpAnimatedHeader.js
@@ -0,0 +1,47 @@
+/**
+ * cbpAnimatedHeader.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+var cbpAnimatedHeader = (function () {
+
+	var docElem = document.documentElement,
+		header = document.querySelector( '.navbar-fixed-top' ),
+		didScroll = false,
+		changeHeaderOn = 200;
+
+	function init() {
+  classie.add( header, 'no-background');
+		window.addEventListener( 'scroll', function( event ) {
+			if( !didScroll ) {
+				didScroll = true;
+				setTimeout( scrollPage, 250 );
+			}
+		}, false );
+	}
+
+	function scrollPage() {
+		var sy = scrollY();
+		if ( sy >= changeHeaderOn ) {
+			classie.add( header, 'navbar-shrink' );
+    classie.remove( header, 'no-background');
+		}
+		else {
+			classie.remove( header, 'navbar-shrink' );
+    classie.add( header, 'no-background');
+		}
+		didScroll = false;
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+	init();
+
+})();
