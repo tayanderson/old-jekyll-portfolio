10739b1d641062badb5c0d97fad87538f85b3686
Taylor Anderson <andersts@indiana.edu>
2015-07-08 19:11:47 -0500

    commit

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,15 +1,15 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-
+//// Check for empty fields
+//if(empty($_POST['name'])  		||
+//   empty($_POST['email']) 		||
+//   empty($_POST['phone']) 		||
+//   empty($_POST['message'])	||
+//   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+//   {
+//	echo "No arguments Provided!";
+//	return false;
+//   }
+//
 //$name = $_POST['name'];
 //$email_address = $_POST['email'];
 //$phone = $_POST['phone'];
